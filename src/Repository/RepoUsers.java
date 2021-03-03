7d58b538ca01a26068e8334ac37bad72370ab751
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-03-03 16:48:12 -0300

    Publicacion de inpus y User Controller

--- src/Repository/RepoUsers.java
+++ src/Repository/RepoUsers.java
@@ -21,5 +21,23 @@ public class RepoUsers {
         return instance;
     }
 
+    public void addUser(User user){
+        this.listUsers.add(user);
+    }
+
+    public void delete(User user){
+        this.listUsers.remove(user);
+    }
+
+    public User getUser(String name, String lastname){
+        User user = null;
+        for (int  i = 0 ; i < this.listUsers.size() ; i++){
+            if(this.listUsers.get(i).getName() == name && this.listUsers.get(i).getLastname() == lastname){
+                user = this.listUsers.get(i);
+            }
+        }
+        return user;
+    }
+
 
 }

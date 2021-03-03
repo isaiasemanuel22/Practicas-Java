5d8f5696ba8f7f9fbdd27625f31c499f1655cc1b
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-03-03 13:26:50 -0300

    Publicando Repositorio

--- src/Repository/RepoUsers.java
+++ src/Repository/RepoUsers.java
@@ -1,2 +1,25 @@
-package Repository;public class RepoUsers {
+package Repository;
+
+import Models.User;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RepoUsers {
+
+    private static RepoUsers instance = null;
+    private List<User> listUsers;
+
+    private RepoUsers (){
+        listUsers = new LinkedList<User>();
+    }
+
+    public RepoUsers getInstance(){
+        if(instance == null){
+            instance = new RepoUsers();
+        }
+        return instance;
+    }
+
+
 }

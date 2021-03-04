6643bd29f0a5bac2fd9dc91cd5dd2ab72823df63
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-03-04 14:27:29 -0300

    actualizacion

--- src/Controllers/UserController.java
+++ src/Controllers/UserController.java
@@ -14,6 +14,7 @@ public class UserController {
     }
 
     public void  createUser(){
+<<<<<<< HEAD
         User newuser = new User();
 
         newuser.setName(new Inputs<String>().input("Ingrese un nombre" ,String.class));
@@ -24,6 +25,18 @@ public class UserController {
 
         newuser.setAge(new Inputs<Integer>().input("Ingrese una edad" , Integer.class));
         System.out.println("edad: " + newuser.getAge());
+=======
+        User usernuevo = new User();
+
+        usernuevo.setName(new Inputs<String>().input("Ingrese un nombre" , "obj"));
+        System.out.println("nombre: " + usernuevo.getName());
+
+        usernuevo.setLastname(new Inputs<String>().input("Ingrese un apellido" , "obj"));
+        System.out.println("apellido: " + usernuevo.getLastname());
+
+        usernuevo.setAge(new Inputs<Integer>().input("Ingrese una edad" , 1));
+        System.out.println("edad: " + usernuevo.getAge());
+>>>>>>> refs/remotes/java/master
     }
 
 
--- src/Utilidaes/Inputs.java
+++ src/Utilidaes/Inputs.java
@@ -8,11 +8,16 @@ import java.util.function.Supplier;
 
 public class Inputs <T> {
 
+<<<<<<< HEAD
+=======
+     private  T t;
+>>>>>>> refs/remotes/java/master
 
      public Inputs(){
 
      }
 
+<<<<<<< HEAD
      public T input (String mensaje , Object obj){
 
          Scanner scan = new Scanner(System.in);
@@ -24,13 +29,29 @@ public class Inputs <T> {
                 String input = scan.nextLine();
                 entrada = (T) input;
                 System.out.println(input);
+=======
+     public T input (String mensaje , T obj){
+         t = obj;
+         Scanner scan = new Scanner(System.in);
+         System.out.println(mensaje);
+         T entrada = null;
+         if ((t.getClass().getName()) == ("obj".getClass().getName())) {
+             try {
+
+                String input = scan.nextLine();
+                entrada = (T) input;
+>>>>>>> refs/remotes/java/master
              }
              catch (Exception e){
                  System.out.println("Ingrese una entrada valida");
              }
          }else{
              try {
+<<<<<<< HEAD
                  System.out.println("error");
+=======
+
+>>>>>>> refs/remotes/java/master
                  Integer  input = scan.nextInt();
                  entrada = (T) input;
 
